1213b7fedd1a1a5cc96138588cb65eea586216e3
JACKSON <hoekit041218@gmail.com>
2025-02-25 12:27:51 +0800

    minor changes : Syntax error

--- client/logis-app/src/App.tsx
+++ client/logis-app/src/App.tsx
@@ -1,7 +1,7 @@
 
 import './index.css'
 import { Button } from './components/ui/button'
-import { Card, CardContent, CardHeader, CardTitle } from "@components/ui/card"
+import { Card, CardContent, CardHeader, CardTitle } from './components/ui/card'
 
 
 
